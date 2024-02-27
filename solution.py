ba164fcd0be1621623413fc4df891deb80bffcbe
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-27 13:19:23 +0000

    Generating the window complete

--- solution.py
+++ solution.py
@@ -6,11 +6,25 @@ WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+def draw(win):
+    win.fill(BLACK)
+    pygame.display.update()
+
 
 def main():
     run = True
 
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(FPS)
+        draw(WIN)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -18,5 +32,5 @@ def main():
     pygame.quit()
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()

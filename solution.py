6d8ffe46e32016881fa554003e17705c4fcf7f04
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-28 12:30:31 +0000

    Displaying the paddles to the screen

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pygame
 
 pygame.init()
-
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
@@ -11,26 +10,51 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-def draw(win):
-    win.fill(BLACK)
-    pygame.display.update()
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+PADDLE_SPEED = 5
 
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+def draw(win, paddles):
+    win.fill(BLACK)
+    for paddle in paddles:
+        paddle.draw(win)
+    pygame.display.update()
 
 def main():
     run = True
 
     clock = pygame.time.Clock()
 
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
     while run:
         clock.tick(FPS)
-        draw(WIN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                break
-    pygame.quit()
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and left_paddle.y > 0:
+            left_paddle.y -= PADDLE_SPEED
+        if keys[pygame.K_DOWN] and left_paddle.y < HEIGHT - left_paddle.height:
+            left_paddle.y += PADDLE_SPEED
+
+        draw(WIN, [left_paddle, right_paddle])
+
+    pygame.quit()
 
 if __name__ == '__main__':
     main()

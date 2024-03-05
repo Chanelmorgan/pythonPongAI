b9091f404c1cebb01db7805c8366cd8cc4a3aaa0
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-05 15:35:53 +0000

    Test ai method

--- NEAT-Pong-Python-main/tutorial.py
+++ NEAT-Pong-Python-main/tutorial.py
@@ -12,8 +12,28 @@ class PongGame:
         self.right_paddle = self.game_right_paddle
         self.ball = self.game.ball
 
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60)  # limits how fast the game goes
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            # allows us to move the left paddle
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up=False)
 
+            game_info = game.loop()
+            print(game_info.left_score, game_info.right_score)
+            game.draw(False, True)  # shows the combined hits
+            pygame.display.update()
 
+        pygame.quit()
 
 
 # setting up the game
@@ -21,24 +41,3 @@ WIDTH, HEIGHT = 700, 500
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game = Game(window, WIDTH, HEIGHT)
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60) # limits how fast the game goes
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-    # allows us to move the left paddle
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        game.move_paddle(left=True, up=True)
-    if keys[pygame.K_s]:
-        game.move_paddle(left=True, up=False)
-
-    game_info = game.loop()
-    print(game_info.left_score, game_info.right_score)
-    game.draw(False, True) # shows the combined hits
-    pygame.display.update()
-
-pygame.quit()
